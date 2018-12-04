c73cfb1162cb20a8e364fbd23850313d09d1f22a
Kevin Markeles <kmarkeles@ebsco.com>
2018-12-04 10:28:37 -0600

    first commit

---
+++ TestJenkinsApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TestJenkinsApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetText());
+        }
+
+        public static string GetText()
+        {
+            return "Hello World!";
+        }
+    }
+}
